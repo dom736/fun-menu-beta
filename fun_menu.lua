04a7918a1606b5bd4709c6c7907c814e2ab7c8e0
dom736 <86428248+dom736@users.noreply.github.com>
2022-09-04 00:52:42 +0200

    Update fun_menu.lua

--- fun_menu.lua
+++ fun_menu.lua
@@ -385,7 +385,7 @@ util.show_corner_help(WELCOM_MESS.. loadingtimeend-loadtimestart ..WELCOM_MESS_2
 
 checkverhttp = false
 function checkver()
-	scriptversion = 0105
+	scriptversion = 0106
 	async_http.init("api.github.com", "/rate_limit", function(output)
 		ratelimit = string.gsub(string.gsub(string.match(output, '"remaining":%d.'), '"remaining":', ""), ",", "")
 		finishedhttp = true
